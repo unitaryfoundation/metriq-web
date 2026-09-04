886d9a438d479d6792315d3432fb3d818a5d55ab
Alessandro Cosentino <cosenal@gmail.com>
2026-09-04 16:59:48 +0200

    Refine front-page tab links

--- test/inline-tab-links.test.js
+++ test/inline-tab-links.test.js
@@ -7,4 +7,6 @@ const indexHtml = readFileSync(new URL('../index.html', import.meta.url), 'utf8'
 test('platform caption links inline tab references to their views', () => {
   assert.match(indexHtml, /<a href="#view=results">Results<\/a>/);
   assert.match(indexHtml, /<a href="#view=benchmarks">Documentation<\/a>/);
+  assert.doesNotMatch(indexHtml, /&laquo;<a href="#view=(?:results|benchmarks)">/);
+  assert.doesNotMatch(indexHtml, /<\/a>&raquo;/);
 });
